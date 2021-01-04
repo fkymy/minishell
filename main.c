ad8669436f58e035164d094775a43215f31310d9
fkymy <yuskefuku@gmail.com>
2021-01-04 20:28:33 +0900

    test: add test locally

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yufukuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/01 18:44:13 by yufukuya          #+#    #+#             */
-/*   Updated: 2021/01/01 18:46:42 by yufukuya         ###   ########.fr       */
+/*   Updated: 2021/01/04 20:20:28 by yufukuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(int argc, char *argv[])
 {
+	(void)argc;
+	(void)argv;
 	printf("よろしくお願いします！\n");
 	return (0);
 }
