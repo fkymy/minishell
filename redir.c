a70ed42c6cbd8878c9ef4bd9712945537b313665
fkymy <yuskefuku@gmail.com>
2021-01-19 19:23:59 +0900

    redir: add test

--- redir.c
+++ redir.c
@@ -6,7 +6,7 @@
 /*   By: yufukuya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 18:14:27 by yufukuya          #+#    #+#             */
-/*   Updated: 2021/01/19 18:36:22 by yufukuya         ###   ########.fr       */
+/*   Updated: 2021/01/19 18:40:20 by yufukuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,51 +19,57 @@
 
 void		handle_redir(t_command *c)
 {
-		int i = 0;
-		int j = 0;
-		int fd = -1;
-		char **newargv = malloc(sizeof(char *) * (c->argc + 1));
+	int i;
+	int j;
+	int fd;
+	char **newargv;
 
-		while (i < c->argc)
+	newargv = malloc(sizeof(char *) * (c->argc + 1));
+	if (!newargv)
+		die(strerror(errno));
+
+	i = 0;
+	j = 0;
+	fd = -1;
+	while (i < c->argc)
+	{
+		if (ft_strcmp(c->argv[i], "<") == 0)
 		{
-			if (ft_strcmp(c->argv[i], "<") == 0)
-			{
-				fd = open(c->argv[i + 1], O_RDONLY);
-				if (fd == -1)
-					die(strerror(errno));
-				dup2(fd, 0);
-				close(fd);
-				free(c->argv[i]);
-				free(c->argv[i + 1]);
-				i += 2;
-			}
-			else if (ft_strcmp(c->argv[i], ">") == 0)
-			{
-				fd = open(c->argv[i + 1], O_WRONLY|O_CREAT|O_TRUNC, 0666);
-				if (fd == -1)
-					die(strerror(errno));
-				dup2(fd, 1);
-				close(fd);
-				free(c->argv[i]);
-				free(c->argv[i + 1]);
-				i += 2;
-			}
-			else if (ft_strcmp(c->argv[i], ">>") == 0)
-			{
-				fd = open(c->argv[i + 1], O_WRONLY|O_CREAT|O_APPEND, 0666);
-				if (fd == -1)
-					die(strerror(errno));
-				dup2(fd, 1);
-				close(fd);
-				free(c->argv[i]);
-				free(c->argv[i + 1]);
-				i += 2;
-			}
-			else
-				newargv[j++] = c->argv[i++];
+			fd = open(c->argv[i + 1], O_RDONLY);
+			if (fd == -1)
+				die(strerror(errno));
+			dup2(fd, 0);
+			close(fd);
+			free(c->argv[i]);
+			free(c->argv[i + 1]);
+			i += 2;
 		}
-		newargv[j] = NULL;
-		free(c->argv);
-		c->argv = newargv;
-
+		else if (ft_strcmp(c->argv[i], ">") == 0)
+		{
+			fd = open(c->argv[i + 1], O_WRONLY|O_CREAT|O_TRUNC, 0666);
+			if (fd == -1)
+				die(strerror(errno));
+			dup2(fd, 1);
+			close(fd);
+			free(c->argv[i]);
+			free(c->argv[i + 1]);
+			i += 2;
+		}
+		else if (ft_strcmp(c->argv[i], ">>") == 0)
+		{
+			fd = open(c->argv[i + 1], O_WRONLY|O_CREAT|O_APPEND, 0666);
+			if (fd == -1)
+				die(strerror(errno));
+			dup2(fd, 1);
+			close(fd);
+			free(c->argv[i]);
+			free(c->argv[i + 1]);
+			i += 2;
+		}
+		else
+			newargv[j++] = c->argv[i++];
+	}
+	newargv[j] = NULL;
+	free(c->argv);
+	c->argv = newargv;
 }
--- test/test.sh
+++ test/test.sh
@@ -20,6 +20,48 @@ make -C ../ > /dev/null
 cp ../minishell .
 chmod 755 minishell
 
+function exec_test_with_files()
+{
+    echo test1 > test1.txt
+    echo test22 > test2.txt
+    echo test333 > test3.txt
+    echo test4444 > test4.txt
+    echo test55555 > test5.txt
+    echo test666666 > test6.txt
+	TEST1=$(echo $@ "; exit" | ./minishell 2>&-)
+	ES_1=$?
+    rm test[1-6].txt
+    echo test1 > test1.txt
+    echo test22 > test2.txt
+    echo test333 > test3.txt
+    echo test4444 > test4.txt
+    echo test55555 > test5.txt
+    echo test666666 > test6.txt
+	TEST2=$(echo $@ "; exit" | bash 2>&-)
+	ES_2=$?
+    rm test[1-6].txt
+	if [ "$TEST1" == "$TEST2" ] && [ "$ES_1" == "$ES_2" ]; then
+		printf " $GREEN%s$RESET" "[OK] "
+	else
+		printf " $RED%s$RESET" "[KO] "
+	fi
+	printf "$CYAN \"$@\" $RESET"
+	if [ "$TEST1" != "$TEST2" ]; then
+		echo
+		echo
+		printf $RED"Your output : \n%.20s\n$RED$TEST1\n%.20s$RESET\n" "-----------------------------------------" "-----------------------------------------"
+		printf $WHITE"Expected output : \n%.20s\n$WHITE$TEST2\n%.20s$RESET\n" "-----------------------------------------" "-----------------------------------------"
+	fi
+	if [ "$ES_1" != "$ES_2" ]; then
+		echo
+		echo
+		printf $RED"Your exit status : $RED$ES_1$RESET\n"
+		printf $WHITE"Expected exit status : $WHITE$ES_2$RESET\n"
+	fi
+	echo
+	sleep 0.1
+}
+
 function exec_test()
 {
 	TEST1=$(echo $@ "; exit" | ./minishell 2>&-)
@@ -50,11 +92,54 @@ function exec_test()
 
 # Mandatory Part
 
+# ; Separator
 exec_test 'echo hello'
 exec_test 'echo hello world'
 exec_test 'echo hello ; echo world'
 exec_test 'echo hello ; echo world'
 exec_test 'echo 01234567890123456789 ; echo ; echo ; echo'
+
+# | Pipe
 exec_test 'ls | wc | wc'
 exec_test 'ls | wc | wc ; echo test'
 exec_test 'ls | wc | wc ; echo test | wc'
+
+# > < >> Redirection
+
+# < redirection shoud fail if file does not exist
+exec_test 'cat < nonexistingfile'
+exec_test 'echo hello > test.txt ; cat < nonexistingfile < test.txt'
+rm test.txt
+
+# < redirection should
+exec_test_with_files 'wc < test1.txt < test2.txt < test3.txt < test4.txt < test5.txt ; cat test[1-6].txt'
+
+# > redirection should redirect output to file
+exec_test 'echo > test.txt ; cat test.txt ; rm test.txt'
+exec_test 'echo hello > test.txt world ; cat test.txt'
+
+# > redirection should create non-existing file
+exec_test 'echo non-existing file > nonexist ; cat nonexist ; rm nonexist'
+
+# > redirection should work with multiple files
+exec_test_with_files 'echo aaa > test1.txt > test2.txt > test3.txt > test4.txt > test5.txt > test6.txt ; cat test[1-6].txt'
+
+# >> redirection should create file
+exec_test 'rm test1.txt ; echo aaa >> test1.txt ; echo bbb >> test1.txt ; cat test1.txt'
+
+# >> redirection should work with multiple files
+exec_test_with_files 'echo aaa >> test1.txt >> test2.txt >> test3.txt ; echo bbb >> test1.txt >> test2.txt >> test3.txt ; cat test[1-3].txt'
+
+# redirections whould work in mix
+exec_test_with_files 'wc < test1.txt < test2.txt > test3.txt > test4.txt < test5.txt > test6.txt'
+
+exec_test_with_files 'cat > test2.txt > test3.txt > test4.txt < test1.txt'
+
+exec_test_with_files 'echo | cat | wc > test.txt < test6.txt ; cat test.txt'
+
+exec_test_with_files 'echo test666666 >> test6.txt | head -n 1 < test6.txt > test1.txt ; wc > test2.txt < test1.txt ; cat test2.txt'
+
+# redirections and pipes
+exec_test_with_files 'cat test1.txt | cat < test2.txt'
+exec_test_with_files 'cat < test1.txt | cat > test2.txt ; cat test2.txt'
+
